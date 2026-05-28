574fe09c9bfb3027c16365bac58a5d114932f1cc
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2026-05-28 17:35:53 +0700

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://www.ulumfr.my.id';
-    
+
     return [
         {
             url: baseUrl,
